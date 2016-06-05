f79a301551fc478f033b3d6cc9d177fa1b03f9b7
Joey Holland <joeyholland15@Joeys-MacBook-Pro.local>
2016-06-05 11:52:57 -0700

    Introduces new route to load team rosters

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const cheerio = require('cheerio');
 
 const router = express.Router();
 
+//will eventually factor these out into a controller
 router.get('/rankings', function(req, res) {
   var rankings = [];
   url = 'http://collegefootball.ap.org/poll'
@@ -21,6 +22,32 @@ router.get('/rankings', function(req, res) {
       })
     }
     res.send(200, rankings);
+  })
+})
+
+router.get('/roster/:team', function(req, res) {
+  var players = [];
+  var id = req.params.team;
+  url = 'http://www.cfbstats.com/2015/team/' + id + '/roster.html';
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html, {
+        normalizeWhitespace: true,
+      });
+      $('table.team-roster').children().each(function(i, obj) {
+        var i = $(this);
+        var player = {
+          no: i.children().eq(0).text(),
+          name: i.children().eq(1).text(),
+          pos: i.children().eq(2).text(),
+          year: i.children().eq(3).text(),
+          ht: i.children().eq(4).text(),
+          wt: i.children().eq(5).text(),
+        }
+        players.push(player);
+      });
+    }
+    res.send(200, players);
   })
 })
 
